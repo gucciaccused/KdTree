b9f0c9b9edcbc8fd45928a045b8bbfbcc57659a5
Björn Halldór Helgason <bjornhalldor@Bjorns-MacBook-Pro.local>
2014-10-17 14:40:30 +0000

    Skrifaði test

--- src/s3/KdTree.java
+++ src/s3/KdTree.java
@@ -268,18 +268,72 @@ public class KdTree {
     }
     
 
-    /*******************************************************************************
-     * Test client
-     ******************************************************************************/
+    public static Point2D randomPoint(){
+        double x = StdRandom.uniform(0, 1);
+        double y = StdRandom.uniform(0, 1);
+        return new Point2D(x,y);
+    }
+    
     public static void main(String[] args) {
-    	In in = new In();
-    	
-    	KdTree set = new KdTree();
-        for (int i = 0; !in.isEmpty(); i++) {
-            double x = in.readDouble(), y = in.readDouble();
-            set.insert(new Point2D(x, y));
+        In in10k = new In("/Users/bjornhalldor/Dropbox/HR_Tolvunarfraedi/Haustonn_2014/Reiknirit/Skilaverkefni_3/packet/SomeInputs/input10K.txt");
+        In in100k = new In("/Users/bjornhalldor/Dropbox/HR_Tolvunarfraedi/Haustonn_2014/Reiknirit/Skilaverkefni_3/packet/SomeInputs/input100K.txt");
+        In in1m = new In("/Users/bjornhalldor/Dropbox/HR_Tolvunarfraedi/Haustonn_2014/Reiknirit/Skilaverkefni_3/packet/SomeInputs/input1M.txt");
+        Out out = new Out();
+        
+        int numberOfNearestPointsTested = 400;
+        double nearestTime10k = 0;
+        double nearestTime100k = 0;
+        double nearestTime1m = 0;
+        
+        
+        // TESTING 10K
+        PointSET set10k = new PointSET();
+        for (int i = 0; !in10k.isEmpty(); i++) {
+            double x = in10k.readDouble(), y = in10k.readDouble();
+            set10k.insert(new Point2D(x, y));
         }
         
-        set.draw();
-    }
+        Stopwatch time10k = new Stopwatch();
+        for (int i = 0; i < numberOfNearestPointsTested; i++){
+            Point2D nearestTest = randomPoint();
+            set10k.nearest(nearestTest);
+            nearestTime10k += time10k.elapsedTime();
+        }
+        nearestTime10k = nearestTime10k/numberOfNearestPointsTested;
+        
+        // TESTING 100K
+        PointSET set100k = new PointSET();
+        for (int i = 0; !in100k.isEmpty(); i++) {
+            double x = in100k.readDouble(), y = in100k.readDouble();
+            set10k.insert(new Point2D(x, y));
+        }
+        
+        Stopwatch time100k = new Stopwatch();
+        for (int i = 0; i < numberOfNearestPointsTested; i++){
+            Point2D nearestTest = randomPoint();
+            set100k.nearest(nearestTest);
+            nearestTime100k += time100k.elapsedTime();
+        }
+        nearestTime100k = nearestTime100k/numberOfNearestPointsTested;
+        
+        // TESTING 1 M
+        PointSET set1m = new PointSET();
+        for (int i = 0; !in1m.isEmpty(); i++) {
+            double x = in1m.readDouble(), y = in1m.readDouble();
+            set1m.insert(new Point2D(x, y));
+        }
+        
+        Stopwatch time1m = new Stopwatch();
+        for (int i = 0; i < numberOfNearestPointsTested; i++){
+            Point2D nearestTest = randomPoint();
+            set1m.nearest(nearestTest);
+            nearestTime1m += time1m.elapsedTime();
+        }
+        nearestTime1m = nearestTime1m/numberOfNearestPointsTested;
+        
+        
+        DecimalFormat numberFormat = new DecimalFormat("#0.00000000000000");
+        out.println("Average time it takes to run nearest in a tree the size of 10K: " + numberFormat.format(nearestTime10k));
+        out.println("Average time it takes to run nearest in a tree the size of 100K: " + numberFormat.format(nearestTime100k));
+        out.println("Average time it takes to run nearestin a tree the size of 1M: " + numberFormat.format(nearestTime1m));
 }
--- src/s3/PointSET.java
+++ src/s3/PointSET.java
@@ -103,58 +103,73 @@ public class PointSET {
         }
     }
 
-    public static void main(String[] args) {
-        In in = new In();
-        Out out = new Out();
-        int nrOfRecangles = in.readInt();
-        int nrOfPointsCont = in.readInt();
-        int nrOfPointsNear = in.readInt();
-        RectHV[] rectangles = new RectHV[nrOfRecangles];
-        Point2D[] pointsCont = new Point2D[nrOfPointsCont];
-        Point2D[] pointsNear = new Point2D[nrOfPointsNear];
-        for (int i = 0; i < nrOfRecangles; i++) {
-            rectangles[i] = new RectHV(in.readDouble(), in.readDouble(),
-                    in.readDouble(), in.readDouble());
-        }
-        for (int i = 0; i < nrOfPointsCont; i++) {
-            pointsCont[i] = new Point2D(in.readDouble(), in.readDouble());
-        }
-        for (int i = 0; i < nrOfPointsNear; i++) {
-            pointsNear[i] = new Point2D(in.readDouble(), in.readDouble());
-        }
-        PointSET set = new PointSET();
-        for (int i = 0; !in.isEmpty(); i++) {
-            double x = in.readDouble(), y = in.readDouble();
-            set.insert(new Point2D(x, y));
-        }
-        for (int i = 0; i < nrOfRecangles; i++) {
-            // Query on rectangle i, sort the result, and print
-            Iterable<Point2D> ptset = set.range(rectangles[i]);
-            int ptcount = 0;
-            for (Point2D p : ptset)
-                ptcount++;
-            Point2D[] ptarr = new Point2D[ptcount];
-            int j = 0;
-            for (Point2D p : ptset) {
-                ptarr[j] = p;
-                j++;
-            }
-            Arrays.sort(ptarr);
-            out.println("Inside rectangle " + (i + 1) + ":");
-            for (j = 0; j < ptcount; j++)
-                out.println(ptarr[j]);
-        }
-        out.println("Contain test:");
-        for (int i = 0; i < nrOfPointsCont; i++) {
-            out.println((i + 1) + ": " + set.contains(pointsCont[i]));
-        }
-
-        out.println("Nearest test:");
-        for (int i = 0; i < nrOfPointsNear; i++) {
-            out.println((i + 1) + ": " + set.nearest(pointsNear[i]));
-        }
-
-        out.println();
+    public static Point2D randomPoint(){
+        double x = StdRandom.uniform(0, 1);
+        double y = StdRandom.uniform(0, 1);
+        return new Point2D(x,y);
     }
+    
+    public static void main(String[] args) {
+        In in10k = new In("/Users/bjornhalldor/Dropbox/HR_Tolvunarfraedi/Haustonn_2014/Reiknirit/Skilaverkefni_3/packet/SomeInputs/input10K.txt");
+        In in100k = new In("/Users/bjornhalldor/Dropbox/HR_Tolvunarfraedi/Haustonn_2014/Reiknirit/Skilaverkefni_3/packet/SomeInputs/input100K.txt");
+        In in1m = new In("/Users/bjornhalldor/Dropbox/HR_Tolvunarfraedi/Haustonn_2014/Reiknirit/Skilaverkefni_3/packet/SomeInputs/input1M.txt");
+        Out out = new Out();
+        
+        int numberOfNearestPointsTested = 400;
+        double nearestTime10k = 0;
+        double nearestTime100k = 0;
+        double nearestTime1m = 0;
+        
+        
+        // TESTING 10K
+        PointSET set10k = new PointSET();
+        for (int i = 0; !in10k.isEmpty(); i++) {
+            double x = in10k.readDouble(), y = in10k.readDouble();
+            set10k.insert(new Point2D(x, y));
+        }
+        
+        Stopwatch time10k = new Stopwatch();
+        for (int i = 0; i < numberOfNearestPointsTested; i++){
+            Point2D nearestTest = randomPoint();
+            set10k.nearest(nearestTest);
+            nearestTime10k += time10k.elapsedTime();
+        }
+        nearestTime10k = nearestTime10k/numberOfNearestPointsTested;
+        
+        // TESTING 100K
+        PointSET set100k = new PointSET();
+        for (int i = 0; !in100k.isEmpty(); i++) {
+            double x = in100k.readDouble(), y = in100k.readDouble();
+            set10k.insert(new Point2D(x, y));
+        }
+        
+        Stopwatch time100k = new Stopwatch();
+        for (int i = 0; i < numberOfNearestPointsTested; i++){
+            Point2D nearestTest = randomPoint();
+            set100k.nearest(nearestTest);
+            nearestTime100k += time100k.elapsedTime();
+        }
+        nearestTime100k = nearestTime100k/numberOfNearestPointsTested;
+        
+        // TESTING 1 M
+        PointSET set1m = new PointSET();
+        for (int i = 0; !in1m.isEmpty(); i++) {
+            double x = in1m.readDouble(), y = in1m.readDouble();
+            set1m.insert(new Point2D(x, y));
+        }
+        
+        Stopwatch time1m = new Stopwatch();
+        for (int i = 0; i < numberOfNearestPointsTested; i++){
+            Point2D nearestTest = randomPoint();
+            set1m.nearest(nearestTest);
+            nearestTime1m += time1m.elapsedTime();
+        }
+        nearestTime1m = nearestTime1m/numberOfNearestPointsTested;
+        
+        
+        DecimalFormat numberFormat = new DecimalFormat("#0.00000000000000");
+        out.println("Average time it takes to run nearest in a tree the size of 10K: " + numberFormat.format(nearestTime10k));
+        out.println("Average time it takes to run nearest in a tree the size of 100K: " + numberFormat.format(nearestTime100k));
+        out.println("Average time it takes to run nearestin a tree the size of 1M: " + numberFormat.format(nearestTime1m));
 
 }
